3a051976ab676ee67db33c72f3fda9ee8d63be05
Manabu ISHII <manabu.ishii.rb@gmail.com>
2019-09-13 15:39:50 +0900

    Update zatsu-cwl-generator.d

--- zatsu-cwl-generator.d
+++ zatsu-cwl-generator.d
@@ -7,5 +7,5 @@ void main()
     writefln("class: CommandLineTool\ncwlVersion: v1.0\nbaseCommand: cat\n");
     writefln("arguments: []\n");
     writefln("inputs: []\n");
-    writefln("outputs: []\n");
+    writefln("outputs:\n  - id: out\n    type: stdout\nstdout: output.txt\n");
 }
